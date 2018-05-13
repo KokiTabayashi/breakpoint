cbf7393640a2cccb1c48f79884d515013aeb6d04
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-05-12 23:08:33 -0700

    lec 148

---
+++ breakpoint/View/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by Koki Tabayashi on 2018/05/12.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+
+}
