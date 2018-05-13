d327831728cead292a13b90c4a07ff7a0ef1e321
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-05-12 22:18:25 -0700

    lec 147

--- breakpoint/Controller/CreatePostVC.swift
+++ breakpoint/Controller/CreatePostVC.swift
@@ -21,6 +21,7 @@ class CreatePostVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
+        sendBtn.bindToKeyboard()
     }
 
     @IBAction func sendBtnWasPressed(_ sender: Any) {
