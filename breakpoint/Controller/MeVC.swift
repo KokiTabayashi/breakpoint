0b424a3c69099995c8316338eddf371eb3cb8dd6
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-05-12 21:32:55 -0700

    lec 146

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,25 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Koki Tabayashi on 2018/05/12.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+}
