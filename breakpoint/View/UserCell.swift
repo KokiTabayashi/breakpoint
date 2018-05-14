fce8f3ce61995d5f2f360edf7aeedd1fa96f4e8b
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-05-13 17:32:29 -0700

    lec 151

---
+++ breakpoint/View/UserCell.swift
@@ -0,0 +1,34 @@
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by Koki Tabayashi on 2018/05/13.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLbl.text = email
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
